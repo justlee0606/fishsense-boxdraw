afc475db5529da66fd1f1c786794b8bb9355c28d
Justin Lee <justlee006@gmail.com>
2021-02-14 22:04:27 -0800

    Move user control code to main script

--- draw.py
+++ draw.py
@@ -1,40 +1,69 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import matplotlib as mpl
 import numpy as np
 import sys
 from matplotlib.widgets import RectangleSelector
-from user_control import line_select_callback, toggle_selector
 
-# Get paths to RGB and depth image
-try:
-    rgb_img_str = str(sys.argv[1])
-    depth_img_str = str(sys.argv[2])
-except:
-    print("RGB and depth images needed")
-    exit()
+def main():
 
-# Start drawing
-try:
-    # Open RGB image
-    rgb_img = mpimg.imread(rgb_img_str)
+    def line_select_callback(eclick, erelease):
+        """
+        Callback for line selection.
 
-    fig, ax = plt.subplots()
-    ax.set_title(
-        "Click and drag to draw a rectangle.\n"
-        "Press 't' to toggle the selector on and off.")
-    # drawtype is 'box' or 'line' or 'none'
-    toggle_selector.RS = RectangleSelector(ax, line_select_callback,
-                                        drawtype='box', useblit=True,
-                                        button=[1, 3],  # disable middle button
-                                        minspanx=5, minspany=5,
-                                        spancoords='pixels',
-                                        interactive=True)
-    fig.canvas.mpl_connect('key_press_event', toggle_selector)
+        *eclick* and *erelease* are the press and release events.
+        """
+        x1, y1 = eclick.xdata, eclick.ydata
+        x2, y2 = erelease.xdata, erelease.ydata
+        print(f"({x1:3.2f}, {y1:3.2f}) --> ({x2:3.2f}, {y2:3.2f})")
 
-    imgplot = plt.imshow(rgb_img)
-    plt.show()
-except:
-    print("Path not correct or image not found")
-    exit()
+    def toggle_selector(event):
+        if event.key == 't':
+            if RS.active:
+                print(' RectangleSelector deactivated.')
+                RS.set_active(False)
+            else:
+                print(' RectangleSelector activated.')
+                RS.set_active(True)
 
-print("Finished!")
+    # Get paths to RGB and depth image
+    try:
+        rgb_img_str = "images/" + str(sys.argv[1])
+        depth_img_str = "images/" + str(sys.argv[2])
+    except:
+        print("RGB and depth images needed")
+        exit()
+
+    # Start drawing
+    try:
+
+        #Disable toolbar on UI
+        mpl.rcParams['toolbar'] = 'None'
+
+        # Open RGB image
+        rgb_img = mpimg.imread(rgb_img_str)
+        depth_img = mpimg.imread(depth_img_str)
+
+        fig, ax = plt.subplots()
+        ax.set_title(
+            "Click and drag to draw a rectangle.\n"
+            "Press 't' to toggle the selector on and off.")
+        # drawtype is 'box' or 'line' or 'none'
+        RS = RectangleSelector(ax, line_select_callback,
+                                            drawtype='box', useblit=True,
+                                            button=[1],  # only draw using left click
+                                            minspanx=5, minspany=5,
+                                            spancoords='pixels',
+                                            interactive=True)
+        fig.canvas.mpl_connect('key_press_event', toggle_selector)
+
+        imgplot = plt.imshow(rgb_img)
+        plt.show()
+    except:
+        print("Path not correct or image not found")
+        exit()
+
+    print("Finished!")
+
+if __name__ == "__main__":
+    main()
